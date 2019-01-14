ff282a77d0ee7896002c2a93d8158ff64feff52a
jakehamtexas <31600776+jakehamtexas@users.noreply.github.com>
2019-01-13 23:37:50 -0600

    initial commit

---
+++ LoanRepaymentUtility/Validate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanRepaymentUtility
+{
+    public class Validate
+    {
+        public static bool IsValidDollarAmount(string myString)
+        {
+            if (decimal.TryParse(myString, out decimal amount))
+            {
+                bool AmountIsNotNegativeAndHasCorrectNumberDecimalDigits = amount > 0 && !(amount * 100 % 1 > 0);
+                if (AmountIsNotNegativeAndHasCorrectNumberDecimalDigits)
+                {
+                    return true;
+                }
+            }
+            return GiveFalseAndWriteUserPrompt();
+        }
+
+        private static bool GiveFalseAndWriteUserPrompt()
+        {
+            Console.WriteLine("Not a valid payment!");
+            Console.WriteLine();
+            return false;
+        }
+    }
+}
---
+++ LoanRepaymentUtilityTest/ApplyPaymentTest.cs
@@ -0,0 +1,133 @@
+﻿using NUnit.Framework;
+using LoanRepaymentUtility;
+namespace LoanRepaymentUtilityTest
+{
+    class LoanTest
+    {
+        private Loan testLoan = new Loan();
+
+        [SetUp]
+        public void Setup()
+        {
+            testLoan.Principle = 1000;
+            testLoan.InterestQuantity = 50;
+        }
+
+        [Test]
+        public void ApplyPaymentTest_PaymentIsGreaterThanInteret_ReducesPrinciple()
+        {
+            decimal testInput = 100;
+            testLoan.Payment = testInput;
+
+            testLoan.ApplyPayment();
+            decimal actual = testLoan.Principle;
+            decimal expected = 950;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ApplyPaymentTest_PaymentIsGreaterThanInteret_InterestIsZero()
+        {
+            decimal testInput = 100;
+            testLoan.Payment = testInput;
+
+            testLoan.ApplyPayment();
+            decimal actual = testLoan.InterestQuantity;
+            decimal expected = 0;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ApplyPaymentTest_PaymentIsEqualToInterest_PrincipleDoesNotChange()
+        {
+            decimal testInput = testLoan.InterestQuantity;
+            testLoan.Payment = testInput;
+
+            testLoan.ApplyPayment();
+            decimal actual = testLoan.Principle;
+            decimal expected = 1000;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ApplyPaymentTest_PaymentIsEqualToInteret_InterestIsZero()
+        {
+            decimal testInput = testLoan.InterestQuantity;
+            testLoan.Payment = testInput;
+
+            testLoan.ApplyPayment();
+            decimal actual = testLoan.InterestQuantity;
+            decimal expected = 0;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ApplyPaymentTest_PaymentIsLessThanInteret_PrincipleDoesNotChange()
+        {
+            decimal testInput = testLoan.InterestQuantity - 25;
+            testLoan.Payment = testInput;
+
+            testLoan.ApplyPayment();
+            decimal actual = testLoan.Principle;
+            decimal expected = 1000;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ApplyPaymentTest_PaymentIsLessThanInteret_InterestReducesByPayment()
+        {
+            decimal testInput = testLoan.InterestQuantity - 25;
+            testLoan.Payment = testInput;
+
+            testLoan.ApplyPayment();
+            decimal actual = testLoan.InterestQuantity;
+            decimal expected = 25;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ApplyPaymentTest_PaymentIsEqualToBalance_InterestIsZero()
+        {
+            decimal testInput = testLoan.Principle + testLoan.InterestQuantity;
+            testLoan.Payment = testInput;
+
+            testLoan.ApplyPayment();
+            decimal actual = testLoan.InterestQuantity;
+            decimal expected = 0;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ApplyPaymentTest_PaymentIsEqualToBalance_PrincipleIsZero()
+        {
+            decimal testInput = testLoan.Principle + testLoan.InterestQuantity;
+            testLoan.Payment = testInput;
+
+            testLoan.ApplyPayment();
+            decimal actual = testLoan.Principle;
+            decimal expected = 0;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ApplyPaymentTest_PaymentIsGreaterThanBalance_BalanceIsNegative()
+        {
+            decimal testInput = testLoan.Principle + testLoan.InterestQuantity + 1;
+            testLoan.Payment = testInput;
+
+            testLoan.ApplyPayment();
+            decimal actual = testLoan.TotalBalance;
+            decimal expected = -1;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
